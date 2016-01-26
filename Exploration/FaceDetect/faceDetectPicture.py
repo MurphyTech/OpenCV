0593da0155e67f3d7fe800594c82b302410f00e7
David Murphy <david@murphytech.ie>
2016-01-26 10:15:20 +0000

    Create faceDetectPicture.py

---
+++ Exploration/FaceDetect/faceDetectPicture.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+
+# Camera 0 is the integrated web cam on my netbook
+camera_port = 0
+ 
+#Number of frames to throw away while the camera adjusts to light levels
+ramp_frames = 30
+ 
+# Now we can initialize the camera capture object with the cv2.VideoCapture class.
+# All it needs is the index to a camera port.
+camera = cv2.VideoCapture(camera_port)
+ 
+# Captures a single image from the camera and returns it in PIL format
+def get_image():
+ # read is the easiest way to get a full image out of a VideoCapture object.
+ retval, im = camera.read()
+ return im
+ 
+# Ramp the camera - these frames will be discarded and are only used to allow v4l2
+# to adjust light levels, if necessary
+for i in xrange(ramp_frames):
+ temp = get_image()
+print("Taking image...")
+# Take the actual image we want to keep
+camera_capture = get_image()
+
+face_cascade = cv2.CascadeClassifier('/home/david/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('/home/david/opencv/data/haarcascades/haarcascade_eye.xml')
+
+
+img = camera_capture
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
